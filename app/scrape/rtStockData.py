fc2a98ac0fbb56b2fed4e76eb6833620d064d24e
newagemob <isaiah.wright@tutanota.com>
2023-07-21 16:51:57 -0500

    updating timestamps

--- app/scrape/rtStockData.py
+++ app/scrape/rtStockData.py
@@ -13,6 +13,8 @@ Objectives:
   - Return analysis to user
 """
 
+
+import argparse
 import secret
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
@@ -30,6 +32,7 @@ import pandas as pd
 import yfinance as yf
 from bs4 import BeautifulSoup
 import datetime as dt
+import pytz
 from pathlib import Path
 
 app_path = Path(__file__).parent.parent
@@ -42,25 +45,35 @@ class RTStockData:
         self.agent = agent
         self.tickers = tickers
 
-    def combine_ticker_data(self):
+    def get_current_ticker_data(self, ticker, period):
         all_ticker_data = []
 
         for ticker in self.tickers:
-            # # Scrape ticker news via serpAPI
-            # serpapi_prompt = f"Objective: Find the latest popular stock news for {ticker}. Summarize and provide a brief summary of the sentiment surrounding the stock and anything else you would add as a quantitative analyst. Be sure to include current events and any other relevant information. \n\nTicker: {ticker}\n\nQuantitative Analyst Report:"
-            # serpapi_response = self.agent(serpapi_prompt)
-            # print(serpapi_response)
-
             # Scrape ticker price via yfinance API
             ticker_info = yf.Ticker(ticker)
-            ticker_hist_max = ticker_info.history(period="max")
+            current_ticker_data = ticker_info.history(period=period)
+
+            # Convert the Ticker_Price_Data to a list of dictionaries
+            ticker_price_data = []
+            for timestamp, price_data in current_ticker_data.iterrows():
+                stock_date = timestamp.strftime("%Y-%m-%d")
+                data_dict = {
+                    "Timestamp": stock_date,
+                    "Open": price_data["Open"],
+                    "High": price_data["High"],
+                    "Low": price_data["Low"],
+                    "Close": price_data["Close"],
+                    "Volume": price_data["Volume"],
+                    "Dividends": price_data["Dividends"],
+                    "Stock Splits": price_data["Stock Splits"],
+                }
+                ticker_price_data.append(data_dict)
 
             # Create a dictionary to store all the data
             ticker_combined_data = {
                 "Ticker": ticker,
-                # "SerpAPI_Response": f"Current Sentiment: {serpapi_response['output']}",
-                "Ticker_Price_Data": ticker_hist_max,
-                "Timestamp": dt.datetime.now(),
+                "Ticker_Price_Data": ticker_price_data,
+                "Date Collected": dt.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
             }
 
             all_ticker_data.append(ticker_combined_data)
@@ -76,35 +89,63 @@ class RTStockData:
                 index=False,
             )
 
-        # CURRENTLY SKIPPING COMBINED DATA
-        # Convert the list of dictionaries to a DataFrame
-        combined_df = pd.DataFrame(all_ticker_data)
+    def get_historical_ticker_data(self, ticker, period):
+        all_ticker_data = []
 
-        # Save Machine Readable DataFrame for future ML training
-        combined_df.to_json(
-            f"{app_path}/scrape/output/mr_collective_data{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.json"
-        )
-        # Save Human Readable CSV
-        combined_df.to_csv(
-            f"{app_path}/scrape/output/hr_collective_data_{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
-        )
+        for ticker in self.tickers:
+            # Scrape ticker price via yfinance API
+            ticker_info = yf.Ticker(ticker)
+            historical_ticker_data = ticker_info.history(period=period)
 
-        return combined_df
+            # Convert the Ticker_Price_Data to a list of dictionaries
+            ticker_price_data = []
+            for timestamp, price_data in historical_ticker_data.iterrows():
+                stock_date = timestamp.strftime("%Y-%m-%d")
+                data_dict = {
+                    "Timestamp": stock_date,
+                    "Open": price_data["Open"],
+                    "High": price_data["High"],
+                    "Low": price_data["Low"],
+                    "Close": price_data["Close"],
+                    "Volume": price_data["Volume"],
+                    "Dividends": price_data["Dividends"],
+                    "Stock Splits": price_data["Stock Splits"],
+                }
+                ticker_price_data.append(data_dict)
+
+            # Create a dictionary to store all the data
+            ticker_combined_data = {
+                "Ticker": ticker,
+                "Ticker_Price_Data": ticker_price_data,
+                "Date Collected": dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            }
+
+            all_ticker_data.append(ticker_combined_data)
+
+            # Save Machine Readable DataFrame for future ML training
+            ticker_df = pd.DataFrame([ticker_combined_data])
+            ticker_df.to_json(
+                f"{app_path}/scrape/output/historical_mr_scraped_data_{ticker}_{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+            )
+            # Save Human Readable CSV
+            ticker_df.to_csv(
+                f"{app_path}/scrape/output/historical_hr_scraped_data_{ticker}_{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.csv",
+                index=False,
+            )
 
 
 if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ai", action="store_true", help="Use OpenAI and SerpAPI")
+    args = parser.parse_args()
+
+    # Set up OpenAI and SerpAPI if the "--ai" argument is passed
     chat = ChatOpenAI(
         temperature=0,
         openai_api_key=secret.OPENAI_API_KEY,
         model="gpt-3.5-turbo",
     )
-    messages = [
-        SystemMessage(
-            content="You are a quantitative analyst reporting to your boss. You are tasked with analyzing the sentiment surrounding a company's stock price based strictly on the news you find."
-        ),
-    ]
-    # llm = OpenAI(temperature=0, openai_api_key=secret.OPENAI_API_KEY, model="gpt-3.5-turbo")
-
     tools = load_tools(
         ["serpapi", "llm-math"], llm=chat, serpapi_api_key=secret.SERPAPI_KEY
     )
@@ -114,8 +155,9 @@ if __name__ == "__main__":
         agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True,
     )
+
     tickers = ["MSFT", "META", "NVDA", "FUBO", "MBRX"]
 
     rt_stock_data = RTStockData(chat, tools, agent, tickers)
-    combined_data = rt_stock_data.combine_ticker_data()
-    print(combined_data)
+    combined_data = rt_stock_data.get_current_ticker_data(tickers, "1d")
+    rt_stock_data.get_historical_ticker_data(tickers, "1y")
